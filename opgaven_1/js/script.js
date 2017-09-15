8fad4df140da753a0e0da4d97943677d31bf0e2f
steffalon <simon.boerrigter@gmail.com>
2017-09-15 11:59:42 +0200

    update van bootcamp javascript

--- opgaven_1/js/script.js
+++ opgaven_1/js/script.js
@@ -33,9 +33,135 @@ console.log(thirdVarible + " and " + secondVarible);
 
 
 // 5
-console.log(typeof temp);
-
-for (var i = 0; i < 15; i++) {
-  window["firstVarible" + i] = i;
-  console.log(typeof window[i]);
+for(var i = 14; i < 20; i++) {
+    var random_boolean = Math.random() >= 0.5;
+    window["variables" + i] = random_boolean;
+    console.log(window["variables" + i]);
 }
+for(var i = 0; i < 20; i++) {
+    console.log(window["variables" + i] + " : " + typeof(window["variables" + i]))
+}
+
+console.log("--FUNCTIONS--")
+
+function multiply(value, multiplier) {
+  var outcome = value * multiplier;
+  console.log("Outcome is " + outcome);
+}
+
+multiply(Math.floor((Math.random() * 10) + 1), Math.floor((Math.random() * 10) + 1));
+
+function makeOneString(string1, string2) {
+  oneString = string1 + string2;
+  console.log(oneString);
+}
+
+makeOneString("Hello", "World");
+
+//Declaration
+
+function returnDeclatation() {
+  return "I'm returning a value";
+}
+
+console.log(returnDeclatation());
+
+//Expression
+var say = function sayTest() {
+  return "test";
+}
+
+console.log(say());
+
+//Anonymous function
+var anonymousFunction = function() {
+  return "This is an anonymous return value of this function";
+}
+
+console.log(anonymousFunction());
+
+
+document.addEventListener("click", function(){
+  alert("test");
+});
+
+//IIFE
+(function runAuto() {
+  console.log("I'm running automaticly without a call");
+}) ();
+
+
+//Closure
+
+function closure() {
+  var functionVarible = "I am a varible";
+  function extend() {
+    return full = functionVarible + " and something is added here!";
+  }
+  console.log(extend());
+}
+
+closure();
+
+
+//Scope
+var singer = "Harry Chapin";
+function makeVisable() {
+  console.log(singer);
+  singer = "Its just Harry";
+  return singer
+}
+
+singer = makeVisable(singer);
+
+console.log(singer);
+
+
+console.log("--ARAYS--");
+
+//length of array
+
+var games = Array("Hitman", "Sims", "The Isle");
+
+console.log(games);
+
+console.log("Groote van array: " + games.length);
+
+//Push
+var newArray = Array();
+console.log(newArray);
+newArray.push("test");
+console.log(newArray);
+
+//Splice
+var spliceArray = new Array("Ian", "Simon", "Tonje", "Dennis", "Menno", "Margot", "Bart", "Steven");
+
+console.log(spliceArray);
+
+spliceArray.splice(1, 3);
+
+console.log(spliceArray);
+
+spliceArray.pop();
+
+console.log(spliceArray + " = Laatste array string is weggehaald.");
+
+console.log("Pop haalt de laatste value weg dus de tegenhanger is push.");
+
+//Alphabet sort
+
+var sortArray = Array("Ben", "Cynthia", "Astrid", "Dirk");
+
+console.log(sortArray);
+
+sortArray.sort();
+console.log(sortArray);
+
+sortArray.reverse();
+console.log(sortArray);
+
+//Sort array with numbers
+
+var numberArray = Array(2, 5, 6, 1, 7, 9);
+
+console.log(numberArray.sort());
